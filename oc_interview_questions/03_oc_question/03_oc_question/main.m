84700fb9a0ff22a52eea971d4dd6cdd33a173575
ruanshutian <1204970556@qq.com>
2022-08-01 17:58:38 +0800

    oc

--- oc_interview_questions/03_oc_question/03_oc_question/main.m
+++ oc_interview_questions/03_oc_question/03_oc_question/main.m
@@ -8,6 +8,7 @@
 #import <Foundation/Foundation.h>
 //#include "NSObject+TestClass.h"
 //#include "NSString+stringExtension.h"
+//https://github.com/zlfyuan/Objective-C--Interview-questions/blob/main/OC%20%E9%9D%A2%E8%AF%95%E9%A2%98.md
 
 
 int main(int argc, const char * argv[]) {
@@ -18,9 +19,9 @@ int main(int argc, const char * argv[]) {
 //        TestClass *testClass = [[TestClass alloc] init];
 //        [testClass test];
 //        [testClass test2];
-        NSString *example = @"hello";
-        
-        [example test];
+//        NSString *example = @"hello";
+//
+//        [example test];
         
     }
     return 0;
